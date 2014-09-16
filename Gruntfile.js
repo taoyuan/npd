8453c8ad90b37f1d9b3ca6beda69475c713134da
taoyuan <torworx@gmail.com>
2014-09-16 14:56:15 +0800

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = function (grunt) {
+  // Show elapsed time at the end
+  require('time-grunt')(grunt);
+  
+  // load all grunt tasks matching the `grunt-*` pattern
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    mochaTest: {
+      options: {
+        reporter: 'spec',
+        ui: 'bdd'
+      },
+      test: {
+        src: ['test/**/*.js']
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'mochaTest']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'mochaTest']
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
+
+};
---
+++ README.md
@@ -0,0 +1,27 @@
+# adm [![NPM Version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url]
+
+> Application Deployment Manager
+
+## Installation
+
+```bash
+$ npm install adm -g
+```
+
+## Documentation
+
+_(Coming soon)_
+
+## Examples
+
+_(Coming soon)_
+
+## License
+
+Copyright (c) 2014 Tao Yuan Licensed under the MIT license.
+
+[npm-url]: https://www.npmjs.org/package/adm
+[npm-image]: https://img.shields.io/npm/v/adm.svg?style=flat
+
+[travis-url]: https://travis-ci.org/taoyuan/adm
+[travis-image]: http://img.shields.io/travis/taoyuan/adm.svg?style=flat
