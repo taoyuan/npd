95b88ae3740062f691cdd744636e3af45789731a
taoyuan <torworx@gmail.com>
2015-09-09 12:22:02 +0800

    0.2.0

--- bin/npd.js
+++ bin/npd.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 
-require("../lib/cli")(process.argv.slice(2), function(err) {
-    if (err) {
-        console.err(err);
-        process.exit(1);
-    }
+require("../lib/cli")(process.argv.slice(2), function (err) {
+  if (err) {
+    console.err(err);
+    process.exit(1);
+  }
 });
